f8c4612b8e115d6e41b528f46e5553b5154f489c
Memmott <jmemmott@EVDIMED>
2018-02-15 14:20:19 -0700

    add

--- homepage/array.js
+++ homepage/array.js
@@ -21,11 +21,19 @@ document.getElementById("displayArray").onclick = function displayArray() {
     if (numberArray == 0) {
         alert("The array is empty");
     }
-    //console.log(numberArray.join());
+    console.log(numberArray);
     var output = document.getElementById("output_array");
     var count = 0;
     output.value = "Your array contains the following numbers:"
     for (count; count < numberArray.length; count++) {
         output.value = output.value + " " + numberArray[count];        
     }
+}
+
+document.getElementById("resetField").onclick = function resetArray() {
+    var output = document.getElementById("output_array");
+    output.value = '';
+    numberArray.length = 0;
+
+    console.log(numberArray);
 }
