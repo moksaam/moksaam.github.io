a1f1740a0e70b7a0ff2b312a5607d5ab0ae3b5c0
Memmott <jmemmott@EVDIMED>
2018-02-15 13:31:53 -0700

    add

--- homepage/array.js
+++ homepage/array.js
@@ -22,7 +22,7 @@ document.getElementById("displayArray").onclick = function displayArray() {
     //console.log(numberArray.join());
     var output = document.getElementById("output_array");
     var count = 0;
-    output.value += output.value + "Your array contains the following numbers: "
+    output.value = "Your array contains the following numbers: "
     for (count; count < numberArray.length; count++) {
         
         output.value += output.value + numberArray[count] + ", ";
