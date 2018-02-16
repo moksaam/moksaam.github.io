7e1042bd4b5670c4f1588caaf7bfaf5831a54c74
moksaam <mem16001@byui.edu>
2018-02-15 22:51:00 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -12,15 +12,15 @@ request.send();
 
 
 
-document.getElementById("parseTraitsBtn").onclick = function displayJson(arr) {
+document.getElementById("parseTraitsBtn").onclick = function displayJson(my_races_JSON_obj) {
     var i, j, x = 0;
     var jsonOutput = document.getElementById("output_json");
 
     console.log("displayJson button pushed");
-    for (i; i < arr.length; i++) {
-        jsonOutput.value += "<h3>" + arr.races[i] + "</h3>"; 
-        for (j; j < arr.races[i].length; j++) {
-            jsonOutput.value += "<p>" + arr.races[i][j] + "</p>"
+    for (i; i < my_races_JSON_obj.length; i++) {
+        jsonOutput.value += "<h3>" + my_races_JSON_obj.races[i] + "</h3>"; 
+        for (j; j < my_races_JSON_obj.races[i].length; j++) {
+            jsonOutput.value += "<p>" + my_races_JSON_obj.races[i][j] + "</p>"
         }       
     }
     
