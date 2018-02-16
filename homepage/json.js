427faba486181e1e32378da5cad58d19986973e6
moksaam <mem16001@byui.edu>
2018-02-15 22:33:59 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -5,34 +5,31 @@ request.onload = function () {
     var my_races_JSON_obj = JSON.parse(request.responseText);
     
     console.log(my_races_JSON_obj);
-    //displayTraits(my_races_JSON_obj);
+    //displayJson(my_races_JSON_obj);
 }
 
 request.send();
 
 
 
-document.getElementById("parseTraitsBtn").onclick = function displayTraits(my_races_JSON_obj) {
+document.getElementById("parseTraitsBtn").onclick = function displayJson(arr) {
     var i, j, x = 0;
     var jsonOutput = document.getElementById("output_json");
 
-    for (i in my_races_JSON_obj.races) {
-        x += "<h2>" + my_races_JSON_obj.races[i].name + "</h2>";
-        for (j in my_races_JSON_obj.races[i].desc) {
-            x += my_races_JSON_obj.races[i].desc[j] + "";
-        }
+    for (i; i < arr.length; i++) {
+        x += "<h3>" + arr.races[i] + "</h3>";        
     }
     jsonOutput.value = x;
 }
 
-document.getElementById("parseJsonBtn").onclick = function parseJsonFile(my_races_JSON_obj) {
+document.getElementById("parseJsonBtn").onclick = function parseJsonFile(arr) {
     var i, j, x = 0;
     var jsonOutput = document.getElementById("output_json");
 
-    for (i in my_races_JSON_obj.races) {
-        x += "<h2>" + my_races_JSON_obj.races[i].name + "</h2>";
-        for (j in my_races_JSON_obj.races[i].desc) {
-            x += my_races_JSON_obj.traits[i].desc[j] + "";
+    for (i in arr.races) {
+        x += "<h2>" + arr.races[i].name + "</h2>";
+        for (j in arr.races[i].desc) {
+            x += arr.traits[i].desc[j] + "";
         }
     }
     jsonOutput.value = x;
