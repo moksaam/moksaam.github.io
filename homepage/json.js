9a5ca70bce9c83f30a1730246267a030befa5864
Memmott <jmemmott@EVDIMED>
2018-02-16 09:56:59 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -15,6 +15,25 @@ request.onload = function () {
         }
         jsonOutput.innerHTML = x;
     }
+
+    document.getElementById("parseJsonBtn").onclick = function parseJsonFile(my_races_JSON_obj) {
+        var i, j, k, x = 0;
+        var jsonOutput = document.getElementById("output_json");
+    
+        console.log("parseJsonFile button pushed");
+        for (i in my_races_JSON_obj.races) {
+            for (j in my_races_JSON_obj.races[i]) {
+                
+                for (k in my_races_JSON_obj.races[i].traits) {
+                    x += "<p>" + my_races_JSON_obj.races[i].traits[k] + "</p>";
+                }
+            }
+           
+            
+        }
+        jsonOutput.innerHTML = x;
+    }
+    
     console.log(my_races_JSON_obj);
     //displayJson(my_races_JSON_obj);
     return my_races_JSON_obj;
@@ -34,12 +53,17 @@ document.getElementById("parseJsonBtn").onclick = function parseJsonFile(my_race
     for (i in my_races_JSON_obj.races) {
         for (j in my_races_JSON_obj.races[i]) {
             
-            for (k in my_races_JSON_obj.races[i][j]) {
-                x += "<p>" + my_races_JSON_obj.races[i][j][k] + "</p>";
+            for (k in my_races_JSON_obj.races[i].traits) {
+                x += "<p>" + my_races_JSON_obj.races[i].traits[k] + "</p>";
             }
         }
        
         
     }
     jsonOutput.innerHTML = x;
+}
+
+document.getElementById("resetFieldBtn").onclick = function resetField() {
+    var field = document.getElementById("output_json");
+    field.innerHTML = '';
 }
