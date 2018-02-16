25c23dfb873fb323024c3e0505c1097ace3b0e86
Memmott <jmemmott@EVDIMED>
2018-02-16 09:17:40 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -3,7 +3,19 @@ request.overrideMimeType("application/json");
 request.open('GET', 'jsonex.json', true);
 request.onload = function () {
     var my_races_JSON_obj = JSON.parse(request.responseText);
+    document.getElementById("parseTraitsBtn").onclick = function displayJson(my_races_JSON_obj) {
+        var i, j, x = 0;
+        var jsonOutput = document.getElementById("output_json");
     
+        console.log("displayJson button pushed");
+        for (i; i < my_races_JSON_obj.length; i++) {
+            jsonOutput.innerHTML += "<h3>" + my_races_JSON_obj.races[i] + "</h3>"; 
+            for (j; j < my_races_JSON_obj.races[i].length; j++) {
+                jsonOutput.innerHTML += "<p>" + my_races_JSON_obj.races[i][j] + "</p>"
+            }       
+        }
+        
+    }
     console.log(my_races_JSON_obj);
     //displayJson(my_races_JSON_obj);
     return my_races_JSON_obj;
@@ -13,19 +25,7 @@ request.send();
 
 
 
-document.getElementById("parseTraitsBtn").onclick = function displayJson(my_races_JSON_obj) {
-    var i, j, x = 0;
-    var jsonOutput = document.getElementById("output_json");
 
-    console.log("displayJson button pushed");
-    for (i; i < my_races_JSON_obj.length; i++) {
-        jsonOutput.innerHTML += "<h3>" + my_races_JSON_obj.races[i] + "</h3>"; 
-        for (j; j < my_races_JSON_obj.races[i].length; j++) {
-            jsonOutput.innerHTML += "<p>" + my_races_JSON_obj.races[i][j] + "</p>"
-        }       
-    }
-    
-}
 
 document.getElementById("parseJsonBtn").onclick = function parseJsonFile(my_races_JSON_obj) {
     var i, j, x = 0;
