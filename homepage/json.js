5187079a2d1483fbe70dfa9a6a439c3078534907
moksaam <mem16001@byui.edu>
2018-02-15 22:49:01 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -16,6 +16,7 @@ document.getElementById("parseTraitsBtn").onclick = function displayJson(arr) {
     var i, j, x = 0;
     var jsonOutput = document.getElementById("output_json");
 
+    console.log("displayJson button pushed");
     for (i; i < arr.length; i++) {
         jsonOutput.value += "<h3>" + arr.races[i] + "</h3>"; 
         for (j; j < arr.races[i].length; j++) {
@@ -29,6 +30,7 @@ document.getElementById("parseJsonBtn").onclick = function parseJsonFile(arr) {
     var i, j, x = 0;
     var jsonOutput = document.getElementById("output_json");
 
+    console.log("parseJsonFile button pushed");
     for (i in arr.races) {
         x += "<h2>" + arr.races[i].name + "</h2>";
         
