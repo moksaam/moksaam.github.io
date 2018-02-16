fba3b5606f453c9734720829c356baf2f9e2b621
Memmott <jmemmott@EVDIMED>
2018-02-16 10:52:53 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -4,7 +4,7 @@ request.open('GET', 'jsonex.json', true);
 request.onload = function () {
     var my_races_JSON_obj = JSON.parse(request.responseText);
     document.getElementById("parseTraitsBtn").onclick = function displayJson() {
-        var i, j, x = 0;
+        var i, j, k, x = 0;
         var jsonOutput = document.getElementById("output_json");
     
         console.log("displayJson button pushed");
@@ -31,12 +31,12 @@ req.open('GET', 'jsonex.json', true);
 req.onload = function () {
     var my_races_JSON_obj = JSON.parse(req.responseText);
     document.getElementById("parseJsonBtn").onclick = function parseJsonFile(my_races_JSON_obj) {
-        var i, j, k, x = 0;
+        var i, j, x = 0;
         var jsonOutput = document.getElementById("output_json");
 
         console.log("parseJsonFile button pushed");
         for (i in my_races_JSON_obj.races) {
-            x += "<h3>" + my_races_JSON_obj.races[i] + "</h3>";
+            //x += "<h3>" + my_races_JSON_obj.races[i] + "</h3>";
             for (j in my_races_JSON_obj.races[i]) {
                 x += "<p>" + my_races_JSON_obj.races[i][j] + "</p>";
             }       
