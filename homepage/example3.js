1d3c7b0fd47d7b77837ac6f6e40e10ead61c73e8
Memmott <jmemmott@EVDIMED>
2018-02-22 09:47:52 -0700

    add

--- homepage/example3.js
+++ homepage/example3.js
@@ -8,4 +8,25 @@ window.onload = function () {
         var change_demo = document.getElementById("demo_ondblClick");
         change_demo.innerHTML = "Amazing. You have double-clicked your way to victory!";
     }
+
+    document.addEventListener("dragstart", function(event) {
+        event.dataTransfer("Text", event.target.id);
+    });
+
+    document.addEventListener("drag", function(event) {
+        document.getElementById("drag_txt").innerHTML = "Dragging has been engaged!";
+    });
+
+    document.addEventListener("dragover", function(event) {
+        event.preventDefault();
+    });
+
+    document.addEventListener("drop", function(event) {
+        event.preventDefault();
+        if (event.target.className == "dragdemo") {
+            var data = event.dataTransfer("Text");
+            event.target.appendChild(document.getElementById(data));
+            document.getElementById("drag_txt").innerHTML = "Dragging has ended. Drop successful.";
+        }
+    })
 }
