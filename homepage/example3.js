e8f41034596c2fd9fc16334e2b5608286747cc5e
Memmott <jmemmott@EVDIMED>
2018-02-22 10:45:47 -0700

    add

--- homepage/example3.js
+++ homepage/example3.js
@@ -1,16 +1,19 @@
 window.onload = function () {
     document.getElementById("demo_onClick").onclick = function () {
+        console.log("demo_onClick clicked!");
         var change_demo = document.getElementById("demo_onClick");
         change_demo.innerHTML = "Congratulations, you have deafeated the first challenge!";
     }
 
     document.getElementById("demo_ondblClick").ondblclick = function () {
+        console.log("demo_ondblClick clicked!");
         var change_demo = document.getElementById("demo_ondblClick");
         change_demo.innerHTML = "Amazing. You have double-clicked your way to victory!";
     }
 
     document.addEventListener("dragstart", function(event) {
-        event.dataTransfer.setData("Text", event.target.id);
+        console.log("dragstart");
+        event.dataTransfer.setData("text/plain", event.target.id);
     });
 
     document.addEventListener("drag", function(event) {
@@ -18,15 +21,18 @@ window.onload = function () {
     });
 
     document.addEventListener("dragover", function(event) {
+        console.log("dragover");
         event.preventDefault();
     });
 
     document.addEventListener("drop", function(event) {
+        console.log("Drop!");
         event.preventDefault();
         if (event.target.className == "dragdemo") {
-            var data = event.dataTransfer.getData("Text");
+            var data = event.dataTransfer.getData("text");
             event.target.appendChild(document.getElementById(data));
             document.getElementById("drag_txt").innerHTML = "Dragging has ended. Drop successful.";
+            event.dataTransfer.clearData();
         }
-    })
+    });
 }
