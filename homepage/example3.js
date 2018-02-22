69475c9985cd9ca210a07f0a8411e92353d0972f
Memmott <jmemmott@EVDIMED>
2018-02-22 09:16:05 -0700

    add

--- homepage/example3.js
+++ homepage/example3.js
@@ -3,4 +3,9 @@ window.onload = function () {
         var change_demo = document.getElementById("demo_onClick");
         change_demo.innerHTML = "Congratulations, you have deafeated the first challenge!";
     }
+
+    document.getElementById("demo_ondblClick").ondblclick = function () {
+        var change_demo = document.getElementById("demo_ondblClick");
+        change_demo.innerHTML = "Amazing. You have double-clicked your way to victory!";
+    }
 }
