8d27c6e8d1bde5de5bd4f8641c5ff606f8573954
moksaam <mem16001@byui.edu>
2018-03-03 11:46:48 -0700

    add

--- homepage/canvasdemo.js
+++ homepage/canvasdemo.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 context.moveTo(0,0);
-context.lineTo(200,100);
+context.lineTo(400,200);
 context.stroke();
