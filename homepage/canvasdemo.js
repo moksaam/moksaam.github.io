81f1c2e4f962db3c8e00fe34969c0dcdded82171
moksaam <mem16001@byui.edu>
2018-03-03 12:55:28 -0700

    add

--- homepage/canvasdemo.js
+++ homepage/canvasdemo.js
@@ -4,10 +4,14 @@ context.moveTo(0,0);
 context.lineTo(400,200);
 context.stroke();
 
-document.getElementById("changeCanvasBtn").addEventListener("click", function changeToCircle() {
+document.getElementById("clearCanvasBtn").addEventListener("click", function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.getElementById("drawCirclesBtn").addEventListener("click", function drawCircle() {
     var canvas = document.getElementById("myCanvas");
     var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    
     context.beginPath();
     context.arc(Math.random() * 400, Math.random() * 200, Math.random() * 100 + 1, 0, 2*Math.PI, false);
     context.stroke();
