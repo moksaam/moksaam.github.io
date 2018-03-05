becb74a7dcf01814c85a92767bab92214efa689e
Memmott <jmemmott@EVDIMED>
2018-03-05 11:44:49 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -4,16 +4,15 @@ request.open('GET', 'jsonex.json', true);
 request.onload = function parseJson() {
     var my_races_JSON_obj = JSON.parse(request.responseText);
     document.getElementById("parseTraitsBtn").onclick = function displayJson() {
-        var i, j, k, x = 0;
+        var i, j, k, l, x = 0;
         var jsonOutput = document.getElementById("output_json");
     
         console.log("displayJson button pushed");
         
         for (i in my_races_JSON_obj.races) {
-            for (j in my_races_JSON_obj.races[i]) {                
-                for (k in my_races_JSON_obj.races[i].traits[j]) {
-                    console.log(my_races_JSON_obj.races[i].traits[j][k])                                        
-                    x += "<p>" + my_races_JSON_obj.races[i].traits[j][k] + "</p>";
+            for (j in my_races_JSON_obj.races[i]) {                                      
+                    x += "<p>" + my_races_JSON_obj.races[i] + "</p>";
+                    }
                 }
             } 
         }
