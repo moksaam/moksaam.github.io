df2641bcf53baebd5af3425a17c3811e00a7850b
moksaam <mem16001@byui.edu>
2018-03-06 23:19:25 -0700

    add

--- homepage/test.js
+++ homepage/test.js
@@ -17,6 +17,13 @@ function incrementTimer() {
             var mins = Math.floor(secs / 60);
             var secs = Math.floor(time / 10);
             var tenthSec = time % 10;
+
+            if(mins < 10) {
+                mins = "0" + mins;
+            }
+            if(secs < 10) {
+                secs = "0" + secs;
+            }
             document.getElementById("test").innerHTML = mins + ":" + secs + ":" + "0" + tenthSec;
             incrementTimer();
         }, 100);
