81398e523d458986de4008cc409085c8e9133f7d
Memmott <jmemmott@EVDIMED>
2018-03-08 14:56:08 -0700

    add

--- homepage/wk5.js
+++ homepage/wk5.js
@@ -1,17 +1,19 @@
+//Setup variables
 var storageBtn = document.getElementById("localStorageBtn");
 var retrieveBtn = document.getElementById("retrieveLocalStorageBtn");
 var display = document.getElementById("display").value;
+var input = document.getElementById("localStorageIn").value;
 
 storageBtn.onclick = function addToStorage() {
     //Store Locally
-    localStorage.setItem("demo", document.getElementById("localStorageIn").value);
+    localStorage.setItem("input", document.getElementById("localStorageIn").value);
 }
 
 retrieveBtn.onclick = function retriveFromStorage() {
     //Check for Browser Support
     if (typeof(Storage) !== "undefined") {
         // Retrieve
-        display = localStorage.getItem("demo");
+        display = localStorage.getItem("input");
     } else {
         display = "Sorry, your browser does not support Web Storage...";
     }
