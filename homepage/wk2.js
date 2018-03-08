c6e7fde72d1ee4c5468b44d9012ca22cf4703c50
moksaam <mem16001@byui.edu>
2018-03-07 22:55:25 -0700

    add

---
+++ homepage/wk2.js
@@ -0,0 +1,14 @@
+//Object Creation Function JS
+
+function showEquipment() {
+
+    var equipment = {};
+    equipment.head = "Black Top Hat";
+    equipment.body = "Black Tie Tuxedo";
+    equipment.back = "Flowing Cape";
+    equipment.hands = "White Gloves";
+    equipment.feet = "Black Dress Shoes";
+
+    var magicianOutfit = "Head Worn: " + equipment.head + "Body Worn: " + equipment.body + "Back Worn: " + equipment.back + "Hands Worn: " + equipment.hands + "Feet Worn: " + equipment.feet;
+    document.getElementById("magicianOutfit").innerHTML += magicianOutfit;
+};
