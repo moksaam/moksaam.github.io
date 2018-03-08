d20bdcaad154cc500b15edb5223c270e57674acf
Memmott <jmemmott@EVDIMED>
2018-03-08 15:40:00 -0700

    add

--- homepage/wk5.js
+++ homepage/wk5.js
@@ -3,6 +3,11 @@ var storageBtn = document.getElementById("localStorageBtn");
 var retrieveBtn = document.getElementById("retrieveLocalStorageBtn");
 var display = document.getElementById("display");
 var input = document.getElementById("localStorageIn");
+var arrStrgBtn = document.getElementById("localStorageArrBtn");
+var retArrBtn = document.getElementById("retrieveLocalStorageArrBtn");
+var arrDisplay = document.getElementById("displayArray");
+var arrInput = document.getElementById("demoArrayInput");
+var inputArray = [];
 
 storageBtn.onclick = function addToStorage() {
     //Store Locally
@@ -16,5 +21,23 @@ retrieveBtn.onclick = function retriveFromStorage() {
         display.innerHTML = localStorage.getItem("demoInput");
     } else {
         display.innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
+
+arrStrgBtn.onclick = function addToStorage() {
+    //Add to array
+    inputArray.push(arrInput.value);
+    console.log("Added " + arrInput.value + " to array.");
+    //Store Locally
+    localStorage.setItem("demoArrayInput", JSON.stringify(arrInput.value));
+}
+
+retArrBtn.onclick = function retriveFromStorage() {
+    //Check for Browser Support
+    if (localStorage) {
+        // Retrieve
+        arrDisplay.innerHTML = localStorage.getItem("demoArrayInput");
+    } else {
+        arrDisplay.innerHTML = "Sorry, your browser does not support Web Storage...";
     }
 }
