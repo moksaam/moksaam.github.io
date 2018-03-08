5c97e4b4710f0ff8a16ff71195958ac31cef5b53
moksaam <mem16001@byui.edu>
2018-03-07 23:19:52 -0700

    add

--- homepage/wk2.js
+++ homepage/wk2.js
@@ -12,3 +12,24 @@ document.body.onload = function showEquipment() {
     document.getElementById("magicianOutfit").innerHTML += magicianOutfit;
 };
 
+//Inheritance function
+document.body.onload = function adventurers() {
+
+    function adventurer(_class, _race, _weapon, _armor) {
+        this._class  = _class;
+        this._race   = _race;
+        this._weapon = _weapon;
+        this._armor  = _armor;
+    };
+
+    adventurer();
+
+    var warrior = new adventurer ('Warrior', 'Human', 'Longsword', 'Chainmail');
+    var wizard = new adventurer ('Wizard', 'Dwarf', 'Staff', 'Red Robes');
+    var cleric = new adventurer ('Cleric', 'Human', 'Mace', 'Scalemail');
+    var bard = new adventurer ('Bard', 'Elf', 'Rapier', 'Leather');
+    var thief = new adventurer ('Thief', 'Halfling', 'Hand Crossbow', 'Padded Leather');
+    
+    var quest = "The young " + warrior._race + " " + warrior._class + "gathered his friends the " + wizard._race +" " + wizard._class + ", the " + cleric._race + " " + cleric._class + ", the " + bard._race + " " + bard._class + ", and the " + thief._race + " " + thief._class + " to go on an adventure. He grabbed his trusty " + warrior._weapon + " and donned his " + warrior._armor + " and then they hit the road.";
+    document.getElementById("adventurers").innerHTML = quest;
+};
