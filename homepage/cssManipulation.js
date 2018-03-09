21fcae0e28dca80ded1a384cf70973210cfcdcd3
Memmott <jmemmott@EVDIMED>
2018-03-09 14:50:06 -0700

    add

--- homepage/cssManipulation.js
+++ homepage/cssManipulation.js
@@ -1,6 +1,16 @@
 // This function will change the background of all the divs on this page.
 document.getElementById("changeAllDivsColor").onclick = function colorChangeDivs() {
     var divs = document.querySelectorAll("div");
-    for (var i = 0; i < divs.length; i++)
-    divs[i].style.backgroundColor = "violet";
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.backgroundColor = "violet";
+    }
+}
+
+// This function will change the font of all P elements
+document.getElementById("changePFont").onclick = function changePFont() {
+    var p_elem = document.querySelectorAll("p");
+    for (var i = 0; i < p_elem.length; i++) {
+        p_elem[i].style.fontSize = "24px";
+        p_elem[i].style.fontFamily = "Verdana";
+    }
 }
