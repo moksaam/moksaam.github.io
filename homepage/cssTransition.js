a11581c8e7a8259d564bbd419ff1c2c2adbc8bc2
Memmott <jmemmott@EVDIMED>
2018-03-09 13:52:30 -0700

    add

--- homepage/cssTransition.js
+++ homepage/cssTransition.js
@@ -1,6 +1,6 @@
 var div = document.getElementById("transition3");
 
-div.onmouseover = function transitionDiv() {
+div.onclick = function transitionDiv() {
     div.style.transition = "width 4s, height 4s, font-size 5s";
     div.setAttribute("style", "height: 20px");
     div.setAttribute("style", "width 20px");
