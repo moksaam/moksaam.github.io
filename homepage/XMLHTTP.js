59fa0925b2fa28af4e08c631e4e8d73a0f2ce2a0
Memmott <jmemmott@EVDIMED>
2018-03-09 15:31:34 -0700

    add

--- homepage/XMLHTTP.js
+++ homepage/XMLHTTP.js
@@ -1,6 +1,6 @@
 // This function will use XMLHTTP to get a file from the web
 var xmlhttp = new XMLHttpRequest();
-if ()'withCredentials' in xmlhttp) {
+if ('withCredentials' in xmlhttp) {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.responseText);
