12504315f940aa7a64f55b29fbdb0ca45ade6595
moksaam <mem16001@byui.edu>
2018-03-08 21:44:11 -0700

    add

--- homepage/css.js
+++ homepage/css.js
@@ -1,6 +1,6 @@
 var div = document.getElementById("example3");
 
-function animateDiv() {
+div.onclick = function animateDiv() {
     div.style.animation = "changes 5s 3";
 }
 
@@ -9,8 +9,8 @@ div.addEventListener("animationRepeat", rptFunction);
 div.addEventListener("animationEnd", stopFunction);
 
 function startFunction() {
-    this.style.height = 400;
-    this.style.width = 400;
+    this.style.height = "400px";
+    this.style.width = "400px";
 }
 
 function rptFunction() {
@@ -20,6 +20,6 @@ function rptFunction() {
 }
 
 function stopFunction() {
-    this.style.height = 200;
-    this.style.width = 200;
+    this.style.height = "200px";
+    this.style.width = "200px";
 }
