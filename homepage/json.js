617278fe686d4b1ddeb87bccfa6c4093eb62c52f
Memmott <jmemmott@EVDIMED>
2018-03-09 12:44:43 -0700

    add

--- homepage/json.js
+++ homepage/json.js
@@ -35,31 +35,8 @@ function parseJson(my_races_JSON_obj) {
     }
     console.log(my_races_JSON_obj);
 }
-
 request.send();
 
-
-var req = new XMLHttpRequest();
-req.overrideMimeType("application/json");
-req.open('GET', 'jsonex.json', true);
-req.onload = function parseTraits() {
-    var my_races_JSON_obj = JSON.parse(req.responseText);
-    document.getElementById("parseTraitsBtn").onclick = function parseJsonFile(my_races_JSON_obj) {
-        var i, j, x = 0;
-        var jsonOutput = document.getElementById("output_json");
-
-        console.log("parseJsonFile button pushed");
-        for (i in my_races_JSON_obj.races) {
-            x += "<h3>" + my_races_JSON_obj.races[i] + "</h3>";
-            for (j in my_races_JSON_obj.races[i]) {
-                x += "<p>" + my_races_JSON_obj.races[0][j] + "</p>";
-            }       
-        }
-        jsonOutput.innerHTML = x;
-    }
-}
-
-req.send();
 
 document.getElementById("resetFieldBtn").onclick = function resetField() {
     var field = document.getElementById("output_json");
