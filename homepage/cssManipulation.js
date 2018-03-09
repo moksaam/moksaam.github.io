0bf97c32cd8b2a582cdd3cb2d1321f3e9484c81f
Memmott <jmemmott@EVDIMED>
2018-03-09 14:32:54 -0700

    add

--- homepage/cssManipulation.js
+++ homepage/cssManipulation.js
@@ -2,5 +2,5 @@
 var divs = document.querySelectorAll("div");
 
 document.getElementById("changeAllDivsColor").onclick = function colorChangeDivs() {
-    divs.style.backgroundColor = violet;
+    divs.style.backgroundColor = "violet";
 }
