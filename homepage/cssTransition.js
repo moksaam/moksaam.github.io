9ab427d33f291ea143a764e24008b8e75fda7a7a
Memmott <jmemmott@EVDIMED>
2018-03-09 13:40:49 -0700

    add

--- homepage/cssTransition.js
+++ homepage/cssTransition.js
@@ -2,5 +2,6 @@ var div = document.getElementById("transition3");
 
 div.onclick = function transitionDiv() {
     div.style.transition = "width 4s, height 4s, font-size 5s";
+    div.style.touchAction = "height 20px, width 20px, font-size 8px";
 }
 
