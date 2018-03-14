56b53331d5856eeb087c1e8a567e1763f37f0c9b
Memmott <jmemmott@EVDIMED>
2018-03-14 13:02:49 -0700

    add

--- homepage/cssTransform.js
+++
@@ -1,5 +0,0 @@
-// This function will simply reset our transformed div
-document.getElementById("resetDiv").onclick = function resetDiv() {
-    var oldDiv = document.getElementById("scaleY_1.5");
-    oldDiv.style.transform = "scaleY(1)";
-}
