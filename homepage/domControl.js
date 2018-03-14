7b0ef053109bd8200a8cb5aa30c094cbe6d91447
Memmott <jmemmott@EVDIMED>
2018-03-14 10:34:54 -0700

    add

--- homepage/domControl.js
+++ homepage/domControl.js
@@ -1,7 +1,11 @@
 // Creating our functions to alter the DOM
-document.getElementById("createElementBtn").onclick = function CreateElement() {
+document.getElementById("createElementBtn").onclick = function createElement() {
     // Creating a new DIV
     var newDiv = document.createElement("div");
     newDiv.className = "childDiv";
     document.getElementById("divContainer").appendChild(newDiv);
+}
+
+document.getElementById("appendChildBtn").onclick = function appendChild() {
+    //
 }
