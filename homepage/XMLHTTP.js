0eff9eb622848041429f64d6a5cd4c02a6da0c1c
moksaam <mem16001@byui.edu>
2018-03-13 20:02:52 -0700

    add

--- homepage/XMLHTTP.js
+++ homepage/XMLHTTP.js
@@ -14,7 +14,7 @@ if ('withCredentials' in xmlhttp) {
 function parseJSON(obj) {
     var display = document.getElementById("display");
     for (var i = 0; i < obj.length; i++) {
-        display.innerHTML += obj[i].forms;
+        display.innerHTML += obj.forms[i];
     }
     console.log(obj);
 }
