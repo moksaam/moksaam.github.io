b6221cc0a171c7794589ed7aeff75cde19dcafb5
Memmott <jmemmott@EVDIMED>
2018-03-14 10:41:46 -0700

    add

--- homepage/domControl.js
+++ homepage/domControl.js
@@ -2,10 +2,21 @@
 document.getElementById("createElementBtn").onclick = function createElement() {
     // Creating a new DIV
     var newDiv = document.createElement("div");
+    newDiv.id = "addText";
     newDiv.className = "childDiv";
     document.getElementById("divContainer").appendChild(newDiv);
 }
 
 document.getElementById("appendChildBtn").onclick = function appendChild() {
-    //
+    var newP = document.createElement("P");
+    var newText = document.createTextNode("Here is our new p element.");
+    newP.appendChild(newText);
+    document.getElementById("addText").appendChild(newP);
+}
+
+function bodyDiv() {    
+    // Creating a new DIV
+    var newDiv = document.createElement("div");
+    newDiv.className = "secondChildDiv";
+    document.body.appendChild(newDiv);
 }
