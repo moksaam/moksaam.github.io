3ec8b05a5d05c9861a2bcd238b19034367fde102
moksaam <mem16001@byui.edu>
2018-03-14 20:24:36 -0700

    add

--- homepage/trigger.js
+++ homepage/trigger.js
@@ -1,6 +1,7 @@
 // Creating the code to move our box left to right
 // Creating variable for out div box
-var moveBox = document.getElementsByClassName("left_to_right_box")[0];
+var moveBox = document.getElementsByClassName("left_to_right_box")[0],
+    sqrToCir = document.getElementsByClassName("square_to_circle")[0];
 
 document.getElementsByClassName("playBtn")[0].onclick = function boxMove() {
     if (this.innerHTML === 'Play') {
@@ -12,5 +13,18 @@ document.getElementsByClassName("playBtn")[0].onclick = function boxMove() {
             marginLeft = computedStyle.getPropertyValue('margin-left');
         moveBox.style.marginLeft = marginLeft;
         moveBox.classList.remove('moveLeft');
+    }
+}
+
+document.getElementsByClassName("sqrBtn")[0].onclick = function makeCircle() {
+    if (this.innerHTML === 'Start') {
+        this.innerHTML = 'Stop';
+        sqrToCir.classList.add('circleIt');
+    } else {
+        this.innerHTML = 'Start';
+        var computedStyle = window.getComputedStyle(sqrToCir),
+            transform = computedStyle.getPropertyValue('transform');
+        sqrToCir.style.transform = transform;
+        sqrToCir.classList.remove('circleIt');
     }
 }
